b4783a848c6b4a7f906259792f97003e706c6afe
Nemanja <nemanja.d.krstic@gmail.com>
2025-12-25 16:39:16 -0800

    Env fix

--- src/socket.ts
+++ src/socket.ts
@@ -1,3 +1,3 @@
 import { createWebSocketClient } from "./utils/websocket-client";
 
-export const socket = createWebSocketClient(import.meta.env.VITE_API_URL);
+export const socket = createWebSocketClient(import.meta.env.VITE_API_SOCKET_URL);
